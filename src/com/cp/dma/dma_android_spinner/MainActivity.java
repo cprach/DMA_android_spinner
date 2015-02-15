24caa34b8cac5f41181b2bf7e9e38d96f5dc1a03
cprach <rachael.colley@computerpower.edu.au>
2015-02-15 16:59:48 +1100

    initial

--- src/com/cp/dma/dma_android_spinner/MainActivity.java
+++ src/com/cp/dma/dma_android_spinner/MainActivity.java
@@ -4,13 +4,40 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements OnItemSelectedListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+
+
+
+	}
+
+	public void populateSpinners() {
+		Spinner countries_spinner = (Spinner)findViewById(R.id.countries_spinner);
+		countries_spinner.setOnItemSelectedListener(this);
+		ArrayAdapter <CharSequence> countriesAdapter = ArrayAdapter.createFromResource(this, 
+				R.array.countries_list, android.R.layout.simple_spinner_item);
+		countriesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		countries_spinner.setAdapter(countriesAdapter);
+
+
+		Spinner airlines_spinner = (Spinner)findViewById(R.id.airlines_spinner);
+		airlines_spinner.setOnItemSelectedListener(this);
+		ArrayAdapter <CharSequence> airlinesAdapter = ArrayAdapter.createFromResource(this, 
+				R.array.airlines_list, android.R.layout.simple_spinner_item);
+		airlinesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		airlines_spinner.setAdapter(airlinesAdapter);
 	}
 
 	@Override
@@ -30,5 +57,31 @@ public class MainActivity extends ActionBarActivity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+		if (arg2 > 0) { 
+
+			String selection = (String)arg0.getItemAtPosition(arg2);
+
+			if (arg0.getId() == R.id.countries_spinner) {
+				TextView txtCurrentCityName = (TextView)findViewById(R.id.txtCurrentCityName);
+				txtCurrentCityName.setText(selection);
+			}
+
+			if (arg0.getId() == R.id.airlines_spinner) {
+				TextView txtCurrentAirlineName = (TextView)findViewById(R.id.txtCurrentAirlineName);
+				txtCurrentAirlineName.setText(selection);
+			}
+
+		}
+
+	}
+
+	@Override
+	public void onNothingSelected(AdapterView<?> arg0) {
+		// TODO Auto-generated method stub
+
 	}
 }
